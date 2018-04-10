92f4dd5b96929fa860aa681248b48c4cf5099f40
Satyam <sharma.satyam@outlook.com>
2018-04-10 03:22:27 -0400

    Update db.sql
    Didn't realize you had also put the create tables statements for the three tables. Anway, I wrote them again. Choose/Edit however you want.
    note: references are maintained between tables.

--- db.sql
+++ db.sql
@@ -1,3 +1,64 @@
 create TABLE coins (coin_id int AUTO_INCREMENT PRIMARY KEY, symbol varchar(10), name varchar(255));
 create TABLE exchanges (id int auto_increment primary key, name varchar(255),region varchar(255));
 create TABLE coinexchanges (id int auto_increment primary key, coin_id int, exchange_id int);
+
+
+///Satyam
+CREATE TABLE exchanges (
+name VARCHAR(10) NOT NULL,
+region VARCHAR(10) NOT NULL,
+exchangeID INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
+
+
+CREATE TABLE coins (
+symbol VARCHAR(5) NOT NULL,
+full_name VARCHAR(10) NOT NULL,
+coinID INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
+
+
+CREATE TABLE price_feed (
+coin INT, 
+price FLOAT NOT NULL,
+date DATETIME NOT NULL,
+exchange INT,
+price_feedID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+
+FOREIGN KEY (coin) REFERENCES coins(coinID),
+FOREIGN KEY (exchange) REFERENCES exchanges(exchangeID)
+);
+
+
+
+CREATE TABLE wallets (
+type VARCHAR(5) NOT NULL,
+value FLOAT NOT NULL,
+walletID INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
+
+
+CREATE TABLE transactions (
+type ENUM('Buy', 'Sell') NOT NULL,
+transactionPrice FLOAT NOT NULL,
+transactionQty INT NOT NULL,
+transactionTotal INT NOT NULL,
+price_feedID INT, 
+originWallet INT, 
+destinationWallet INT,
+
+FOREIGN KEY (price_feedID) REFERENCES price_feed(price_feedID),
+FOREIGN KEY (originWallet) REFERENCES wallets(walletID),
+FOREIGN KEY (destinationWallet) REFERENCES wallets(walletID)
+);
+
+
+CREATE TABLE coin_exchange (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+coin_id INT,
+exchange_id INT,
+
+FOREIGN KEY (coin_id) REFERENCES coins(coinID),
+FOREIGN KEY (exchange_id) REFERENCES exchanges(exchangeID)
+);
+////
