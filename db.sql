63cac57660feca32994ef5504228e68fc8f62f74
khelif96 <khelif96@gmail.com>
2018-04-10 14:17:59 -0400

    Renamed variables to follow convention and added a view

--- db.sql
+++ db.sql
@@ -1,26 +1,26 @@
 CREATE TABLE exchanges (
 name VARCHAR(10) NOT NULL,
 region VARCHAR(10) NOT NULL,
-exchangeID INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+exchange_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
 
 CREATE TABLE coins (
 symbol VARCHAR(5) NOT NULL,
-full_name VARCHAR(10) NOT NULL,
-coinID INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+name VARCHAR(30) NOT NULL,
+coin_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
 
 CREATE TABLE price_feed (
-coin INT, 
+coin INT,
 price FLOAT NOT NULL,
 date DATETIME NOT NULL,
 exchange INT,
-price_feedID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+priceFeed_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 
-FOREIGN KEY (coin) REFERENCES coins(coinID),
-FOREIGN KEY (exchange) REFERENCES exchanges(exchangeID)
+FOREIGN KEY (coin) REFERENCES coins(coin_id),
+FOREIGN KEY (exchange) REFERENCES exchanges(exchange_id)
 );
 
 
@@ -28,22 +28,23 @@ FOREIGN KEY (exchange) REFERENCES exchanges(exchangeID)
 CREATE TABLE wallets (
 type VARCHAR(5) NOT NULL,
 value FLOAT NOT NULL,
-walletID INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+wallet_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 
 
 CREATE TABLE transactions (
+transaction_id int AUTO_INCREMENT PRIMARY KEY,
 type ENUM('Buy', 'Sell') NOT NULL,
 transactionPrice FLOAT NOT NULL,
 transactionQty INT NOT NULL,
 transactionTotal INT NOT NULL,
-price_feedID INT, 
-originWallet INT, 
+priceFeed_id INT,
+originWallet INT,
 destinationWallet INT,
 
-FOREIGN KEY (price_feedID) REFERENCES price_feed(price_feedID),
-FOREIGN KEY (originWallet) REFERENCES wallets(walletID),
-FOREIGN KEY (destinationWallet) REFERENCES wallets(walletID)
+FOREIGN KEY (priceFeed_id) REFERENCES price_feed(priceFeed_id),
+FOREIGN KEY (originWallet) REFERENCES wallets(wallet_id),
+FOREIGN KEY (destinationWallet) REFERENCES wallets(wallet_id)
 );
 
 
@@ -52,7 +53,8 @@ id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 coin_id INT,
 exchange_id INT,
 
-FOREIGN KEY (coin_id) REFERENCES coins(coinID),
-FOREIGN KEY (exchange_id) REFERENCES exchanges(exchangeID)
+FOREIGN KEY (coin_id) REFERENCES coins(coin_id),
+FOREIGN KEY (exchange_id) REFERENCES exchanges(exchange_id)
 );
-////
+
+create view V_coinexchanges as select ce.id as 'Coin Exchange id', c.name as 'Coin Name', e.name as 'Exchange Name' from coins c inner join coinexchanges ce on ce.coin_id = c.coin_id inner join exchanges e on e.exchange_id = ce.exchange_id;
