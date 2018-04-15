9291a8c7af57a3aefc8ce9ba009bb9d1f6790385
khelif96 <khelif96@gmail.com>
2018-04-14 21:24:20 -0400

    Custom error messages

--- app.js
+++ app.js
@@ -20,47 +20,67 @@ if(process.env.host == undefined || process.env.user == undefined || process.env
     password: process.env.password,
     database: process.env.database
   });
+  var coinExchanges = []
+
 
   con.connect(function(err){
     if(err){
       console.log('ERROR Connecting to DB');
+      console.log(err);
       con.end();
       process.exit(1);
     };
     console.log("Connected To Database");
-    var sql = "show tables";
+    var sql = "select * from V_coinexchanges";
     con.query(sql, function(err,result){
       if(err) throw err;
-      console.log("Tables in DB");
-      for (var table in result) {
-        console.log(result[table].Tables_in_cryptotrading)
+      for (var row in result) {
+        var coinExchange = {
+          'symbol' : result[row].symbol,
+          'exchangeName' : result[row]['Exchange Name']
+        }
+        coinExchanges.push(coinExchange);
       }
-      con.end();
+      getPrices(coinExchanges);
+
+      con.end// const url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + coinList + "&tsyms=USD&e=";
+();
 
     })
   })
 
 }
-// const coinList = 'BTC'
-// const url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + coinList + "&tsyms=USD&e=";
-// const exchangeList = ['Bitfinex','Kraken', 'HitBTC', 'coinone','Bithumb']
-// for (var i = 0 ; i<exchangeList.length; i++) {
-//   axios
-//     .get(url + exchangeList[i])
-//     .then(response => {
-//       console.log(JSON.stringify(response.data));
-//       for (var coin in response.data) {
-//         console.log(
-//           `Exchange: ${exchangeList[i]} ~`,
-//           `Coin: ${coin} -`,
-//           `USD: ${response.data[coin].USD} -`,
-//           `TIME: ${new Date()}`
-//         );    }
-//
-//     })
-//
-//     .catch(error => {
-//       console.log("ERROR " + error);
-//     });
-//
-// }
+
+function getPrices(coinExchanges){
+  var ecount = 0;
+  for (var coinExchange in coinExchanges) {
+    var url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + coinExchanges[coinExchange].symbol + "&tsyms=USD&e="+ coinExchanges[coinExchange].exchangeName;
+      axios
+        .get(url)
+        .then(response => {
+          // console.log(JSON.stringify(response.data));
+          for (var coin in response.data) {
+            // console.log(
+            //   `Exchange: ${coinExchanges[coinExchange].exchangeName} ~`,
+            //   `Coin: ${coinExchanges[coinExchange].symbol} -`,
+            //   `USD: ${response.data[coinExchanges[coinExchange].symbol].USD} -`,
+            //   `TIME: ${new Date()}`
+            // );
+            console.log(coin + " " + JSON.stringify(response.data[coin]))
+            // if(response.data[coinExchanges[coinExchange].symbol].USD == undefined){
+            //   console.log(response.data)
+            //   ecount++;
+            // }
+            }
+
+        })
+
+        .catch(error => {
+          console.log("ERROR " + error);
+          ecount++;
+          console.log(ecount)
+        });
+
+    }
+    console.log("ecount " + ecount)
+  }
